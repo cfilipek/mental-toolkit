bae4a5711cf25bcb6738e3c08c36f76f24d92637
Claire Filipek <cfilipek@sva.edu>
2019-08-07 14:30:11 -0400

    added modal to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from 'react-router-dom'
-import {Navbar} from 'react-bootstrap'
+import {Navbar, Modal, Button} from 'react-bootstrap'
+
 
 //fixed navbar
 //hover border-bottom 2px solid white
 
 
 const Nav = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
       <Navbar
@@ -25,11 +32,25 @@ const Nav = () => {
           id="responsive-navbar-nav"
           className="justify-content-end"
         >
-          <Navbar.Text className="margin-right-nav-emergency">
+          <Navbar.Text className="margin-right-nav-emergency" onClick={handleShow} >
             <NavLink>
               Emergency Resources
             </NavLink>
           </Navbar.Text>
+          <Modal show={show} onHide={handleClose} animation={false}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
           <Navbar.Text className="margin-right-nav">
             <NavLink>
               About
