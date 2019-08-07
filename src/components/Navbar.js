3cc7d60aecd2cd4771a15bd6ed79329a189c8a86
Claire Filipek <cfilipek@sva.edu>
2019-08-07 14:12:44 -0400

    worked on navbar and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,54 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import {Navbar} from 'react-bootstrap'
 
 //fixed navbar
 //hover border-bottom 2px solid white
 
 
-const Navbar = () => {
+const Nav = () => {
   return (
     <div>
-      <div>Logo</div>
-      <nav>
-        <ul>
-          <li>
-           <NavLink to='/'>Children</NavLink>
-          </li>
-          <li>
-          <NavLink to='/todos'>Children</NavLink>
-          </li>
-          <li>
-          <NavLink to='/login'>Children</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        fixed="top"
+        className="blue-nav"
+      >
+        <Navbar.Brand>
+          <NavLink to="/">
+            <img src="https://i.imgur.com/b8coz0D.png" alt="logo"/>
+          </NavLink>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-end"
+        >
+          <Navbar.Text className="margin-right-nav-emergency">
+            <NavLink>
+              Emergency Resources
+            </NavLink>
+          </Navbar.Text>
+          <Navbar.Text className="margin-right-nav">
+            <NavLink>
+              About
+            </NavLink>
+          </Navbar.Text>
+              <Navbar.Text className="margin-right-nav">
+                <NavLink>
+                      Login
+                </NavLink>
+              </Navbar.Text>
+                <Navbar.Text className="margin-right-nav">
+                  <NavLink>
+                      Sign up
+                  </NavLink>
+            </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   )
 }
 
-export default Navbar
+export default Nav
