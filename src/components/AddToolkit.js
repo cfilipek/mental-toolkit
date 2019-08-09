5e0acb8a90a2fc94c4951ae4b28b19d1cd4fcd5f
Claire Filipek <cfilipek@sva.edu>
2019-08-09 12:54:22 -0400

    figured out how to style the modal with styled-components

--- src/components/AddToolkit.js
+++ src/components/AddToolkit.js
@@ -3,6 +3,9 @@ import {connect} from 'react-redux'
 import * as actions from '../store/actions'
 import {Button} from 'react-bootstrap'
 import * as Yup from 'yup';
+import { Formik, Field} from 'formik';
+//https://www.styled-components.com/
+import styled from 'styled-components'
 
 //Yup to define fields of db model
 const ToolkitSchema = Yup.object().shape({
@@ -16,16 +19,38 @@ const ToolkitSchema = Yup.object().shape({
     .required('The category is required.')
 })
 
+//styling modal
+const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: ${({ opened }) =>
+  opened ? 'translate(-50%, -50%)' : 'translate(-50%, -150%)'};
+  visibility: ${({ opened }) => (opened ? 'visible' : 'hidden')};
+  background-color: #4c5ad2;
+  color: white;
+  padding: 20px;
+  border-radius: 25px;`
+
 //add toolkit modal using Formik
 //using Formik: https://blog.bitsrc.io/creating-forms-in-react-with-formik-and-yup-698d09363a22
 const AddToolkit = () => {
   const [isOpened, setisOpened] = useState(false)
   return (
+    <>
     <div className="text-center">
       <Button className="button-blue move-down-button" onClick={()=> setisOpened(true)}>
       Add toolkit item
       </Button>
+      <Modal opened={isOpened} close={()=> setisOpened(false)}>
+        <h3 className="modal-heading">
+          Add your toolkit item
+        </h3>
+        <Formik></Formik>
+
+      </Modal>
     </div>
+    </>
   )
 }
 
