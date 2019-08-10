5bc3e2730fc79c73895526e98969273e4fee60a2
Claire Filipek <cfilipek@sva.edu>
2019-08-10 11:50:31 -0400

    fixing commit/push mistake

--- src/components/Community.js
+++ src/components/Community.js
@@ -11,7 +11,7 @@ const Community = () => {
       <div className="title-white">Community<br/>Toolkits</div>
         <Row>
           <Col sm={4}></Col>
-          <Col className="white-text" sm={4}>This page is currently under construction. Check back soon &hearts;</Col>
+          <Col className="white-text center-text" sm={4}>This page is currently under construction. Check back soon &hearts;</Col>
           <Col sm={4}></Col>
         </Row>
       </div>
